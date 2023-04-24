6f596006dd4d0f5df06af8fcd83119a071e8375e
Neil de la fuente <87651732+Neilus03@users.noreply.github.com>
2023-04-24 11:09:33 +0200

    done

--- ft_get_next_line_utils.c
+++ ft_get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: nde-la-f <nde-la-f@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/22 15:27:32 by nde-la-f          #+#    #+#             */
-/*   Updated: 2023/04/24 11:03:31 by nde-la-f         ###   ########.fr       */
+/*   Updated: 2023/04/24 11:08:57 by nde-la-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,17 @@
 
 ssize_t ft_read(int fd, void *buf, size_t count)
 {
-	
+	read(fd, buf, count); //invent de los guapos`	
+}
+
+size_t ft_strlen(char *str)
+{
+	int	i;
+
+	if (!str)
+		return (0);
+	i = 0;
+	while(str[i])
+		i++;
+	return (i);
 }
