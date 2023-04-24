6af3a49513f66d8295365b431046a4c4a6979b64
Neil de la fuente <87651732+Neilus03@users.noreply.github.com>
2023-04-24 08:56:28 +0200

    done

---
+++ ft_get_next_line.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_get_next_line.h                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nde-la-f <nde-la-f@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/22 15:28:01 by nde-la-f          #+#    #+#             */
+/*   Updated: 2023/04/22 15:34:52 by nde-la-f         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#ifndef FT_GET_NEXT_LINE_H
+# define FT_GET_NEXT_LINE_H
+
+# include <stdio.h>
+# include <unistd.h>
+# include <string.h>
+# include <unistd.h>
+# include <stdarg.h>
+# include <stdint.h>
+# include <stdlib.h>
+
+char	*get_next_line(int fd);
+
+#endif //GET_NEXT_LINE_H
+
