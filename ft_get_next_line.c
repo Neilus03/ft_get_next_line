1b51e22e208dd51b063630f2eeaeede301d0871c
Neil de la fuente <87651732+Neilus03@users.noreply.github.com>
2023-04-30 12:40:31 +0200

    gnl update

--- ft_get_next_line.c
+++ ft_get_next_line.c
@@ -6,10 +6,77 @@
 /*   By: nde-la-f <nde-la-f@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/22 15:26:34 by nde-la-f          #+#    #+#             */
-/*   Updated: 2023/04/30 12:27:15 by nde-la-f         ###   ########.fr       */
+/*   Updated: 2023/04/30 12:34:40 by nde-la-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ft_get_next_line.h"
+
+char	*process_buffer(char **buf, char *newline)
+{
+	char	*result;
+	char	*tmp;
+
+	if (newline)
+	{
+		result = ft_substr(*buf, 0, newline - *buf);
+		tmp = ft_strdup(newline + 1);
+		free(*buf);
+		*buf = tmp;
+	}
+	else
+	{
+		result = ft_strdup(*buf);
+		free(*buf);
+		*buf = NULL;
+	}
+	return (result);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*buf = NULL;
+	ssize_t		bytes_read;
+	char		*newline;
+
+	if (fd < 0 || BUFFER_SIZE < 1)
+		return (NULL);
+	if (!buf)
+		buf = ft_strdup("");
+	if (!buf)
+		return (NULL);
+	newline = ft_find_char(buf, '\n');
+	while (newline == NULL)
+	{
+		bytes_read = read_buffer(fd, &buf);
+		if (bytes_read <= 0)
+			break ;
+		newline = ft_find_char(buf, '\n');
+	}
+	return (process_buffer(&buf, newline));
+}
+
+/*
+int	main(void)
+{
+	char	*line;
+	int		i;
+	int		fd1;
+
+	fd1 = open("tests/fd1.txt");
+	i = 1;
+	while (i < 7)
+	{
+		line = get_next_line(fd1);
+		printf("line [%02d]: %s", i, line);
+		free(line);
+		i++;
+	}
+	close(fd1);
+	return (0);
+}
+*/
+/*
 #include "ft_get_next_line.h"
 
 char	*get_next_line(int fd)
@@ -48,25 +115,5 @@ char	*get_next_line(int fd)
 		buf = NULL;
 	}
 	return (result);
-}
-
-/*
-int	main(void)
-{
-	char	*line;
-	int		i;
-	int		fd1;
-
-	fd1 = open("tests/fd1.txt");
-	i = 1;
-	while (i < 7)
-	{
-		line = get_next_line(fd1);
-		printf("line [%02d]: %s", i, line);
-		free(line);
-		i++;
-	}
-	close(fd1);
-	return (0);
 }
 */
